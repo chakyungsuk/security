104fb68c5dd42e5e79002adf0e1f592b35ca6323
ChaKyungSuk <cha0054@naver.com>
2023-01-30 22:55:26 +0900

    최초 commit!!

---
+++ sequrity/src/test/java/com/cos/sequrity/SequrityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cos.sequrity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SequrityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
